ff6be4eb314550ab65db62e915d46ce9f7074148
A7 <abekawamocha@gmail.com>
2023-09-20 06:16:20 +0900

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+import java.awt.Graphics2D
+import java.awt.Image
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+fun main() {
+    val inputImagePath = "path/to/your/input/image.jpg" // 入力画像のパス
+    val outputImagePath = "path/to/your/output/resized_image.jpg" // 出力画像のパス
+    val newWidth = 200 // 新しい幅
+    val newHeight = 150 // 新しい高さ
+
+    resizeImage(inputImagePath, outputImagePath, newWidth, newHeight)
+}
+
+fun resizeImage(inputPath: String, outputPath: String, newWidth: Int, newHeight: Int) {
+    try {
+        val inputFile = File(inputPath)
+        if (!inputFile.exists()) {
+            println("指定したファイルが存在しません: $inputPath")
+            return
+        }
+
+        val originalImage = ImageIO.read(inputFile)
+        val resizedImage = BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB)
+
+        val g2d: Graphics2D = resizedImage.createGraphics()
+        g2d.drawImage(originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH), 0, 0, null)
+        g2d.dispose()
+
+        val outputFile = File(outputPath)
+        ImageIO.write(resizedImage, "jpg", outputFile)
+
+        println("画像をリサイズして保存しました: $outputPath")
+    } catch (e: Exception) {
+        println("エラーが発生しました: ${e.message}")
+    }
+}
